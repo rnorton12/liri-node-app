89da2f026bcb7c1916dff3d9ad35072c27e675fd
Roy Norton <rnorton12@centurylink.net>
2018-01-10 19:34:16 -0700

    initial version

---
+++ liri.js
@@ -0,0 +1,33 @@
+// At the top of the `liri.js` file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
+
+// 8. Make it so liri.js can take in one of the following commands:
+
+//    * `my-tweets`
+
+//    * `spotify-this-song`
+
+//    * `movie-this`
+
+//    * `do-what-it-says`
+
+// ### What Each Command Should Do
+
+// 1. `node liri.js my-tweets`
+
+//    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+// 2. `node liri.js spotify-this-song '<song name here>'`
+
+//    * This will show the following information about the song in your terminal/bash window
+     
+//      * Artist(s)
+     
+//      * The song's name
+     
+//      * A preview link of the song from Spotify
+     
+//      * The album that the song is from
+
+//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
+   
+//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
