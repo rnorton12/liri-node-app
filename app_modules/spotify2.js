fa0148dfa68a94d960dd2df03cdb7aa2e4f589c6
rnorton12 <rnorton12@centurylink.net>
2018-01-16 21:15:29 +0000

    Create spotify2.js

---
+++ app_modules/spotify2.js
@@ -0,0 +1,60 @@
+//  node liri.js spotify-this-song '<song name here>'`
+//  This will show the following information about the song in your terminal/bash window
+//      * Artist(s)
+//      * The song's name
+//      * A preview link of the song from Spotify
+//      * The album that the song is from
+//  If no song is provided then your program will default to "The Sign" by Ace of Base.
+// ========================================================
+var SpotifyWebApi = require("spotify-web-api-node");
+var log = require("./app_modules/log.js");
+
+var text = ""; // text to log or display
+
+exports.printSongInfo = function (keys, song) {
+    var defaultTrack = "The Sign";
+    var defaultArtist = "Ace of Base";
+
+    // Set necessary parts of the credentials on the constructor
+    var spotifyApi = new SpotifyWebApi({
+        clientId: keys.client_id,
+        clientSecret: keys.client_secret
+    });
+
+    // Get an access token and 'save' it using a setter
+    spotifyApi.clientCredentialsGrant()
+    .then(function (data) {
+        spotifyApi.setAccessToken(data.body['access_token']);
+
+        // Do search using the access token
+        if (song === undefined) {
+            query = "track:" + defaultTrack + " artist:" + defaultArtist;
+        } else {
+            query = "track:" + song;
+        }
+
+        spotifyApi.searchTracks(query)
+            .then(function (data) {
+                // console.log(data.body);
+                // console.log(JSON.stringify(data.body.tracks.items));
+                var trackInfo = data.body.tracks.items;
+                for (var i = 0; i < trackInfo.length; i++) {
+                    text = "";
+                    text += "Artist: " + trackInfo[i].artists[0].name + "\n";
+                    text += "Song: " + trackInfo[i].name + "\n";
+                    text += "Preview Url: " + trackInfo[i].preview_url + "\n";
+                    text += "Album: " + trackInfo[i].album.name;
+                    console.log(text);
+                    log.logData(text);
+                }
+            }, function (err) {
+                text = "Something went wrong1! " + err;
+                console.log(text);
+                log.logData(text);
+            });
+    }, function (err) {
+        text = "Something went wrong2! " + err;
+        console.log(text);
+        log.logData(text);
+    });
+}
